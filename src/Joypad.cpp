7958c29b2eed6d5921d3cd55d3fd375df4dcfb28
SMART SOLUTIONS LABS SAC <ventas@smartsolutionslabs.com>
2025-02-05 09:30:22 -0500

    set right pins

--- src/Joypad.cpp
+++ src/Joypad.cpp
@@ -1,9 +1,9 @@
 #include "Joypad.hpp"
 
 Joypad::Joypad(const char * name, int taskCore) : Module(name, taskCore) {
-	this->floorButtons[0].pin = 17;
-	this->floorButtons[1].pin = 5;
-	this->floorButtons[2].pin = 18;
+	this->floorButtons[0].pin = 16;
+	this->floorButtons[1].pin = 4;
+	this->floorButtons[2].pin = 15;
 
 	this->iterationDelay = 10 / portTICK_PERIOD_MS;
 }
@@ -25,16 +25,18 @@ void Joypad::run(void* data) {
 		// Verify floor button state
 		for(i=0;i<3;i++) {
 			this->floorButtons[i].reading = digitalRead(this->floorButtons[i].pin);  //handle true
-			if ((millis() - this->floorButtons[i].lastDebounceTime) > this->debounceDelay) {
-				this->floorButtons[i].lastDebounceTime = millis();
+		}
 
-				if (this->floorButtons[i].reading == false) {
-					this->machinist->handlePush(i - 1);
-					Serial.printf("Btn %d pressed\n", i);
+		for(i=0;i<3;i++) {
+				if (this->floorButtons[i].reading == false && this->floorButtons[i].lastButtonState == true) {
+					this->machinist->handlePush(i + 1);
+					Serial.printf("Btn %d pressed\n", i + 1);
 				}
-			}
 			this->floorButtons[i].lastButtonState = this->floorButtons[i].reading;
 		}
+		/*if ((millis() - this->floorButtons[i].lastDebounceTime) > this->debounceDelay) {
+				this->floorButtons[i].lastDebounceTime = millis();
+		}*/
 
 		// A little delay to avoid many reads
 		vTaskDelay(this->iterationDelay);
